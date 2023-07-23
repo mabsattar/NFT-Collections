e94ee0eb27a7cea70ec541abc0edd2304fae0fd8
AbSattar <mdabdullsattar@gmail.com>
2023-07-23 06:14:48 +0530

    added functions

--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -17,6 +17,8 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
 
     uint256 public tokenIds;
 
+    uint256 public _price = 0.01 ether;
+
     constructor(string memory _baseURI, address whitelistContract) ERC721("Cryoto Devs", "CD") {
         _baseTokenURI = _baseURI;
         whitelist = IWhitelist(whitelistContract);
@@ -31,6 +33,13 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
 
     function presaleMint() public payable {
         require(presaleStarted && block.timestamp < presaleEnded, "Presale ended");
+        require(whitelist.whitelistAddresses(msg.sender), "You are not in whitelist"); 
+        require(tokenIds < maxTokenIds, "Exceeded limit");
+        require(msg.value >= _price, "Ether sent is not correct");
+
+        tokenIds += 1;
+
+        _safeMint(msg.sender, tokenIds);
 
     }
 
